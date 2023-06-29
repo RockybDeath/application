d45816a38eee46b9053ac0945094fa5af65804ea
RockybDeath <mdnvalery@yahoo.com>
2023-06-29 16:25:36 +0300

    Delete useless destroy subs (pipe async)

--- client/clientApp/src/app/app.component.ts
+++ client/clientApp/src/app/app.component.ts
@@ -1,35 +1,26 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { WebsocketService } from '../websocket/services/websocket.service';
 import { IMessage } from '../models/message.interface';
 import { WebsocketEvents } from '../models/websocket-events';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
   title = 'clientApp';
   public messages$: Observable<IMessage[]>;
   public updateMessages$: Observable<IMessage[]>;
-  private destroySubscription$: Subject<boolean> = new Subject<boolean>();
   constructor(private ws: WebsocketService) {
-    this.messages$ = this.ws
-      .on<IMessage[]>(WebsocketEvents.ON.MESSAGES)
-      .pipe(takeUntil(this.destroySubscription$));
-
-    this.updateMessages$ = this.ws
-      .on<IMessage[]>(WebsocketEvents.ON.UPDATE_TEXTS)
-      .pipe(takeUntil(this.destroySubscription$));
+    this.messages$ = this.ws.on<IMessage[]>(WebsocketEvents.ON.MESSAGES);
+    this.updateMessages$ = this.ws.on<IMessage[]>(
+      WebsocketEvents.ON.UPDATE_TEXTS
+    );
   }
 
   public sendText(): void {
     this.ws.send(WebsocketEvents.SEND.SEND_TEXT, 'Hello');
-  }
-
-  ngOnDestroy(): void {
-    this.destroySubscription$.next(false);
-    this.destroySubscription$.complete();
   }
 }
